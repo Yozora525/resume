faeb896bb6d774cf973e46e3d8957ad3d6c1f8ae
Yozora525 <aqourslove665@gmail.com>
2022-03-23 16:27:51 +0800

    20220323-shiyan 1. 修改背景

---
+++ assets/js/background.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xf02d71,
+    backgroundColor: 0x220b19,
+    points: 18.00,
+    spacing: 13.00
+})
