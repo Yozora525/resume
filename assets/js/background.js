dcf61391a20adeb432921064019d9a070a50674c
Yozora525 <aqourslove665@gmail.com>
2022-03-26 22:24:38 +0800

    20220326-shiyan 1. 匯入slick 2. 規劃版面及應有項目

--- assets/js/background.js
+++ assets/js/background.js
@@ -7,8 +7,8 @@ VANTA.NET({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    color: 0xf02d71,
-    backgroundColor: 0x220b19,
+    color: 0xffe83f,
+    backgroundColor: 0x23153c,
     points: 18.00,
     spacing: 13.00
 })
