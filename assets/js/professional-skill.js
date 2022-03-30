155ac5adc89d3885782cc5c94b07aba5d33cce43
Yozora525 <aqourslove665@gmail.com>
2022-03-30 17:22:55 +0800

    20220330-shiyan 1. 改用sass

--- assets/js/professional-skill.js
+++ assets/js/professional-skill.js
@@ -1,40 +1,40 @@
 var professional = echarts.init(document.getElementById('skill-professional'));
 var option;
 
-const data = [10, 20, 30, 50, 40, 90];
+const data = [85, 70, 80, 70, 80, 75];
 
 option = {
-  xAxis: {
-    max: '100'
-  },
-  yAxis: {
-    type: 'category',
-    data: ['拳擊', '攝影', '寫程式', 'D', 'E', 'F'],
-    inverse: true,
-    animationDuration: 300,
-    animationDurationUpdate: 300,
-    max: 5
-  },
-  series: [
-    {
-      realtimeSort: true,
-      name: 'score',
-      type: 'bar',
-      data: data,
-      label: {
-        show: true,
-        position: 'right',
-        valueAnimation: true
-      }
-    }
-  ],
-  legend: {
-    show: true
-  },
-  animationDuration: 0,
-  animationDurationUpdate: 0,
-  animationEasing: 'linear',
-  animationEasingUpdate: 'linear'
+    xAxis: {
+            max: '100'
+        },
+    yAxis: {
+        type: 'category',
+        data: ['寫程式', '攝影', '拳擊', '踢拳', '綜合格鬥', '泰拳'],
+        inverse: true,
+        animationDuration: 300,
+        animationDurationUpdate: 300,
+        max: 5
+    },
+    series: [
+        {
+            realtimeSort: true,
+            name: 'score',
+            type: 'bar',
+            data: data,
+            label: {
+                show: true,
+                position: 'right',
+                valueAnimation: true
+            }
+        }
+    ],
+    legend: {
+        show: true
+    },
+    animationDuration: 0,
+    animationDurationUpdate: 0,
+    animationEasing: 'linear',
+    animationEasingUpdate: 'linear'
 };
 
 option && professional.setOption(option);
